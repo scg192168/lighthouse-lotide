89376f7c9554778c8392501439124e4d5c0e2b58
Shyamal <scghoshbc@gmail.com>
2023-06-28 19:49:39 -0700

    committed

---
+++ without.js
@@ -0,0 +1,24 @@
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
+const without = function(array1, arrayExclude) {
+  const result = [];
+  for (let i = 0; i < array1.length; i++) {
+    const item = array1[i];
+    if (exclude(item, arrayExclude) === false) {
+      result.push(item);
+    }
+  };
+  return result;
+};
+const exclude = function(item, arrayExclude) {
+  for (let x = 0; x < arrayExclude.length; x++) {
+    const pullOut = arrayExclude[x];
+    if (pullOut === item) {
+      return true;
+    }
+  }
+  return false;
+};
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
