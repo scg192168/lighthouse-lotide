d57fe849edcc537b0e81d808a5266ba5515bf22f
Shyamal <scghoshbc@gmail.com>
2023-06-29 19:16:28 -0700

    committed

--- flatten.js
+++ flatten.js
@@ -1,16 +1,14 @@
-cosnt flatten = function (array) {
-  //create a new array
+const flatten = function(array) {
   var newArray = [];
-  for (let i = 0; i < array[i]; i++) {
+  for (let i = 0; i < array.length; i++) {
     if (Array.isArray(array[i])) {
-      for (let x = 0; x <array[i].length; x++) {
+      for (let x = 0; x < array[i].length; x++) {
         newArray.push(array[i][x]);
       }
     } else {
-      newArray.push(array[i])
+      newArray.push(array[i]);
     }
   }
   return newArray;
 };
-
-cconsole.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 5, 6]
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
