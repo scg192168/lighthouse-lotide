c0fa89201f8ed1090759aabb14194941677721e9
Shyamal <scghoshbc@gmail.com>
2023-07-04 19:43:05 -0700

    committed

---
+++ eqObjects.js
@@ -0,0 +1,28 @@
+function eqObjects(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (!object2.hasOwnProperty(key)) {
+      return false;
+    }
+
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    if (typeof value1 === 'object' && typeof value2 === 'object') {
+      if (!eqObjects(value1, value2)) {
+        return false;
+      }
+    } else if (value1 !== value2) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
