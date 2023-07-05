f6ca42374d1376387c6bb09097843a90abe97c98
Shyamal <scghoshbc@gmail.com>
2023-07-05 10:30:13 -0700

    committed

--- eqObjects.js
+++ eqObjects.js
@@ -1,28 +1,21 @@
 function eqObjects(object1, object2) {
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
-
+  console.log("=====", keys1, keys2);
+  //const animal1 = {legs: 4, eyes: 2, horn: 0, head: 1};
+  //const animal2 = {legs: 4, eyes: 2, horn: 2, head: 1};
   if (keys1.length !== keys2.length) {
     return false;
   }
-
   for (let key of keys1) {
-    if (!object2.hasOwnProperty(key)) {
-      return false;
-    }
-
-    const value1 = object1[key];
-    const value2 = object2[key];
-
-    if (typeof value1 === 'object' && typeof value2 === 'object') {
-      if (!eqObjects(value1, value2)) {
-        return false;
-      }
-    } else if (value1 !== value2) {
+    //console.log("___", key);
+    if (object1[key] !== object2[key]) {
       return false;
     }
   }
-
   return true;
 }
 
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+console.log(eqObjects(shirtObject, anotherShirtObject));
