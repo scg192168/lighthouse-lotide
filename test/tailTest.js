a2b3ee3b51ea9675208e31b6d8c5168ef424eb05
Shyamal <scghoshbc@gmail.com>
2023-07-11 14:10:39 -0700

    committed

---
+++ test/tailTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+
+const tail = function(array) {
+  console.log(array);
+  return array.slice(1);
+};
+console.log(tail([1, 6, 7, 8]));
+console.log(tail(["Hello", "Lighthouse", "Labs"]));
+
+module.exports = assertEqual;
